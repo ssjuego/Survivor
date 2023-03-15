b7ccc07f220b267f05427b7daf00cbe8e5ca2ac0
ssjuego <Sanketh@juegostudioz.com>
2023-03-15 19:11:56 +0530

    First day progress

---
+++ My project (1)/Assets/Scripts/NavMesh.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMesh : MonoBehaviour
+{
+    [SerializeField] Transform location;
+    private NavMeshAgent navMeshAgent;
+    [SerializeField] float distanceToPlayer;
+    [SerializeField] float mandatedDistanceToPlayer = 30f;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position , location.position);
+
+        if (distanceToPlayer <= mandatedDistanceToPlayer)
+        {
+            navMeshAgent.destination = location.position;
+        }
+    }
+}
